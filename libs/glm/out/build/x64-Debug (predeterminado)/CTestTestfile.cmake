cb35822548ccf20a80c6afa71b5f0dcd496f8c4e
Iván Díaz <navi11031999@gmail.com>
2020-10-10 16:38:40 +0200

    por favor

--- libs/glm/out/build/x64-Debug (predeterminado)/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ivan_/source/repos/Wlgen/Kojiman/libs/glm
-# Build directory: C:/Users/ivan_/source/repos/Wlgen/Kojiman/libs/glm/out/build/x64-Debug (predeterminado)
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
--- libs/glm/out/build/x64-Debug (predeterminado)/test/gtx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ivan_/source/repos/Wlgen/Kojiman/libs/glm/test/gtx
-# Build directory: C:/Users/ivan_/source/repos/Wlgen/Kojiman/libs/glm/out/build/x64-Debug (predeterminado)/test/gtx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
