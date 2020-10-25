55e9791d8ca1d220b1c594805b3d3c6fdaaff644
Iván Díaz <navi11031999@gmail.com>
2020-10-25 12:04:23 +0100

    gitignore fix try

--- libs/glm/out/build/x64-Debug (predeterminado)/test/core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Usuario/Ivan/Escritorio/uni/vj/Kojiman/libs/glm/test/core
-# Build directory: D:/Usuario/Ivan/Escritorio/uni/vj/Kojiman/libs/glm/out/build/x64-Debug (predeterminado)/test/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
