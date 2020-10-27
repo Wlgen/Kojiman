f8cb5ac163b28e1d4194300b3f285e9926fc6206
Iván Díaz <navi11031999@gmail.com>
2020-10-27 18:49:05 +0100

    cambio tamaño bola y arreglo colisiones

--- 2DGame/02-Bubble/02-Bubble/State.cpp
+++ 2DGame/02-Bubble/02-Bubble/State.cpp
@@ -3,9 +3,10 @@
 State::State() { actual_state = state::menu; }
 State::~State() {}
 
-void State::init() { actual_state = state::menu; }
+void State::init() { previous_state = actual_state = state::menu; }
 
 void State::changeState() {
+    previous_state = actual_state;
     switch (actual_state) {
         case state::menu:
             actual_state = state::play;
@@ -22,12 +23,24 @@ void State::changeState() {
     }
 }
 
-void State::toCredits() { actual_state = state::credits; }
+void State::toCredits() { 
+    previous_state = actual_state;
+    actual_state = state::credits;
+}
 
-void State::toMenu() { actual_state = state::menu; }
+void State::toMenu() { 
+    previous_state = actual_state;
+    actual_state = state::menu; 
+}
 
-void State::toPlay() { actual_state = state::play; }
+void State::toPlay() { 
+    previous_state = actual_state;
+    actual_state = state::play;
+}
 
-void State::toInst() { actual_state = state::instr; }
+void State::toInst() { 
+    previous_state = actual_state;
+    actual_state = state::instr;
+}
 
 State::state State::getState() { return actual_state; }
